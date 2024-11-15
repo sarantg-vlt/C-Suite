dd0e6f2979590acdf6c5bf3cd77e353f3e65b56c
sarantg-vlt <saran@vulturelines.com>
2024-11-15 16:20:57 +0530

    Update auth_linkedIn_execute.js

--- src/Auth/firebase/auth_linkedIn_execute.js
+++ src/Auth/firebase/auth_linkedIn_execute.js
@@ -2,6 +2,11 @@ const clientId = "86aue2n06v0823";
 const redirectUri = "https://csuite-auth.netlify.app/auth-linkedin-bridge";
 const state = "linkedIn-auth-state-secret";
 const scope = "profile openid email";
+
 export const handleLinkedIn = () => {
-  // const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=${scope}`;
+  // Define the LinkedIn authorization URL
+  const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=${scope}`;
+
+  // Redirect the user to the LinkedIn authorization page
   window.location.href = authUrl;
+};
