54872b12beeefbcac25ec8d07c9defe303b412bb
SUDHAKAR G <115939667+1913176@users.noreply.github.com>
2024-11-18 12:48:22 +0530

    Update auth_linkedIn_execute.js

--- src/Auth/firebase/auth_linkedIn_execute.js
+++ src/Auth/firebase/auth_linkedIn_execute.js
@@ -1,5 +1,5 @@
-const clientId = "86aue2n06v0823";
-const redirectUri = "https://csuite-auth.netlify.app/auth-linkedin-bridge";
+const clientId = "86mel6vthcai4p";
+const redirectUri = "https://c-suite-alpha.vercel.app/auth-linkedin-bridge";
 const state = "linkedIn-auth-state-secret";
 const scope = "profile openid email";
 
