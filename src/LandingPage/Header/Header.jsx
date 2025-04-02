924b7f1cef0faf06fe08b0ef772a2ef00a66a97a
Yakash10 <yakashsenthil1015@gmail.com>
2025-04-02 09:39:36 +0530

    nav

--- src/LandingPage/Header/Header.jsx
+++ src/LandingPage/Header/Header.jsx
@@ -1,9 +1,130 @@
-// Header.jsx
-import React, { useEffect, useRef } from "react";
+// // Header.jsx
+// import React, { useEffect, useRef } from "react";
+// import { Link } from "react-router-dom";
+// import NavSlider from "./NavSlider";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+// import Aos from "aos";
+// import "aos/dist/aos.css";
+// import logo from "./Asset/brand-1.png";
+// import "./Header.css";
+
+// function Header() {
+//   const headerRef = useRef();
+//   const AnchorRef = useRef();
+//   useEffect(() => {
+//     Aos.init();
+//   }, []);
+
+//   useEffect(() => {
+//     const handleScroll = () => {
+//       // Check if headerRef and anchorRef are properly set
+//       if (headerRef.current) {
+//         if (window.scrollY > 0) {
+//           headerRef.current.classList.add("scrolled");
+//         } else {
+//           headerRef.current.classList.remove("scrolled");
+//         }
+//       }
+//     };
+
+//     // Attach the scroll event listener
+//     window.addEventListener("scroll", handleScroll);
+
+//     // Cleanup event listener on unmount
+//     return () => {
+//       window.removeEventListener("scroll", handleScroll);
+//     };
+//   }, []);
+
+//   return (
+//     <header ref={headerRef}>
+//       <nav className="navbar navbar-expand-lg navbar-light">
+//         <a
+//           data-aos="fade-right"
+//           className="navbar-brand ml-5 text-light"
+//           href="/"
+//         >
+//           <img src={logo} alt="" height={"40px"} />
+//         </a>
+//         <button
+//           className="navbar-toggler"
+//           type="button"
+//           // data-toggle="collapse"
+//           data-target="#navbarNav"
+//           aria-controls="navbarNav"
+//           aria-expanded="false"
+//           aria-label="Toggle navigation"
+//         >
+//           <span className="navbar-toggler-icon"></span>
+//         </button>
+//         <div className="collapse navbar-collapse" id="navbarNav">
+//           <ul className="navbar-nav ml-auto">
+//             <li className="nav-item active ml-4">
+//               <a className="nav-link text-light" href="#what" ref={AnchorRef}>
+//                 Why C-Suite?
+//               </a>
+//             </li>
+//             <li className="nav-item active ml-4">
+//               <a
+//                 className="nav-link text-light"
+//                 href="#courses"
+//                 ref={AnchorRef}
+//               >
+//                 Courses
+//               </a>
+//             </li>
+//             <li className="nav-item active ml-4">
+//               <a className="nav-link text-light" href="#why" ref={AnchorRef}>
+//                 Customers
+//               </a>
+//             </li>
+//             <li className="nav-item active ml-4">
+//               <a
+//                 className="nav-link text-light"
+//                 href="#contact"
+//                 ref={AnchorRef}
+//               >
+//                 Contact Us
+//               </a>
+//             </li>
+//           </ul>
+//           <ul className="navbar-nav ml-auto mr-3" data-aos="fade-left">
+//             <li className="nav-item active ml-4">
+//               <Link to="/authentication?form=login">
+//                 <button
+//                   type="button"
+//                   className="btn button-logoin text-light mb-2 mb-md-2 mb-lg-0"
+//                 >
+//                   Log In
+//                 </button>
+//               </Link>
+//             </li>
+//             <li className="nav-item active ml-md-3 ml-4">
+//               <Link to="/authentication?form=signup">
+//                 <button type="button" className="btn btn-light">
+//                   Get Started
+//                 </button>
+//               </Link>
+//             </li>
+//           </ul>
+//         </div>
+//       </nav>
+//       <hr className="tag-size" />
+//     </header>
+//   );
+// }
+
+// export default Header;
+
+
+
+
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import NavSlider from "./NavSlider";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import logo from "./Asset/brand-1.png";
@@ -11,14 +132,14 @@ import "./Header.css";
 
 function Header() {
   const headerRef = useRef();
-  const AnchorRef = useRef();
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   useEffect(() => {
     Aos.init();
   }, []);
 
   useEffect(() => {
     const handleScroll = () => {
-      // Check if headerRef and anchorRef are properly set
       if (headerRef.current) {
         if (window.scrollY > 0) {
           headerRef.current.classList.add("scrolled");
@@ -28,10 +149,7 @@ function Header() {
       }
     };
 
-    // Attach the scroll event listener
     window.addEventListener("scroll", handleScroll);
-
-    // Cleanup event listener on unmount
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
@@ -45,56 +163,53 @@ function Header() {
           className="navbar-brand ml-5 text-light"
           href="/"
         >
-          <img src={logo} alt="" height={"40px"} />
+          <img src={logo} alt="Logo" height="40px" />
         </a>
+
+        {/* Toggle Button */}
         <button
           className="navbar-toggler"
           type="button"
-          // data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
+          onClick={() => setIsNavOpen(!isNavOpen)}
           aria-label="Toggle navigation"
         >
-          <span className="navbar-toggler-icon"></span>
+          <FontAwesomeIcon icon={isNavOpen ? faTimes : faBars} />
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+
+        {/* Navigation Menu */}
+        <div
+          className={`collapse navbar-collapse ${isNavOpen ? "show" : ""}`}
+          id="navbarNav"
+        >
           <ul className="navbar-nav ml-auto">
             <li className="nav-item active ml-4">
-              <a className="nav-link text-light" href="#what" ref={AnchorRef}>
+              <a className="nav-link text-light" href="#what">
                 Why C-Suite?
               </a>
             </li>
             <li className="nav-item active ml-4">
-              <a
-                className="nav-link text-light"
-                href="#courses"
-                ref={AnchorRef}
-              >
+              <a className="nav-link text-light" href="#courses">
                 Courses
               </a>
             </li>
             <li className="nav-item active ml-4">
-              <a className="nav-link text-light" href="#why" ref={AnchorRef}>
+              <a className="nav-link text-light" href="#why">
                 Customers
               </a>
             </li>
             <li className="nav-item active ml-4">
-              <a
-                className="nav-link text-light"
-                href="#contact"
-                ref={AnchorRef}
-              >
+              <a className="nav-link text-light" href="#contact">
                 Contact Us
               </a>
             </li>
           </ul>
+
           <ul className="navbar-nav ml-auto mr-3" data-aos="fade-left">
             <li className="nav-item active ml-4">
               <Link to="/authentication?form=login">
                 <button
                   type="button"
-                  className="btn button-logoin text-light mb-2 mb-md-2 mb-lg-0"
+                  className="btn button-logoin text-light mb-2"
                 >
                   Log In
                 </button>
@@ -110,6 +225,10 @@ function Header() {
           </ul>
         </div>
       </nav>
+
+      {/* NavSlider Component for Mobile Navigation */}
+      <NavSlider isOpen={isNavOpen} onClose={() => setIsNavOpen(false)} />
+
       <hr className="tag-size" />
     </header>
   );
